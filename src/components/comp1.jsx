58baa6577c6b72d5585cbea429701c2d83b4d2a3
Tato Salvi <tatosalvi@gmail.com>
2021-05-08 00:27:36 -0300

    created app skeleton

---
+++ src/components/comp1.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './assets/css/comp1.css'
+
+class Comp1 extends Component {
+  render() {
+      
+    return (
+      <div className="Comp1">
+        [ comp 1 ]
+      </div>
+    );
+  }
+}
+
+export default Comp1;
---
+++ src/components/varPanel.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+let var1 = true
+let color = ""
+if (var1) {color = "red"} else {color = "blue"}
+
+let style = {
+    color: color,
+    borderRadius: 10,
+    overflowY: "auto"
+}
+let style2 = {
+    whiteSpace: "nowrap",
+    width: "100%",
+    overflowX: "scroll",
+    paddingBottom: "40px"
+}
+
+class VarPanel extends Component {
+  render() {
+      
+    return (
+      <div style={style}>
+        
+        <div style={style2}>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+        </div>
+
+        <div style={style2}>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+        </div>
+        <div style={style2}>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+        </div>
+        <div style={style2}>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+        </div>
+        <div style={style2}>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+        </div>
+        <div style={style2}>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+        </div>
+        <div style={style2}>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+            14000 x1 + 35000 x2 + 11000 x3 + slack1 + slack2 + slack3 &lt;= &mt; 1400000<br></br>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VarPanel;
